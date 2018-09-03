d0e2afdc83b1a3f536dcc28398f870cec2159fc8
tsrik007 <34542649+tsrik007@users.noreply.github.com>
2018-09-03 22:04:25 +0200

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key"{}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "aws_ami" {}
+variable "path_to_public_key" {}
+variable "path_to_private_key" {}
+variable "instance_username" {}
+variable "server_count" {}
+variable "subnet_id" {}
+variable "vpc_id" {}
+variable "bastion_host" {}
